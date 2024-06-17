e1652ca39169189838dc2840209a5bb663c20398
Dhruv Yalamanchi <51535244+code1word@users.noreply.github.com>
2024-06-16 20:38:26 -0500

    Update Resume.js

--- client/src/PortfolioContainer/Resume/Resume.js
+++ client/src/PortfolioContainer/Resume/Resume.js
@@ -143,27 +143,27 @@ export default function Resume(props) {
         />
 
         
-        <ResumeHeading
-          heading={"Software Engineer / AI Engineer"}
-          subHeading={"Columbia Business School Startup Incubator"}
-          fromDate={"September 2022"}
-          toDate={"May 2024"}
-          description={
-            "Worked as a developer for two startups at the Columbia Business School"
-          }
-        />
-      <div className="experience-container">    
-        <div className="resume-description-text">
-          <ul>
-            <li>
-              Developed back-end for travel experience platform adopted by 15+ hotels using Node.js; built admin and guest authentication and created APIs for managing reservations and property inforomation; implemented front-end in React and utilized Redux to manage state for user login and recommendations
-            </li>
-            <li>
-              Developed mobile app using React Native for non-English speaking patients to discover accessible clinical trials; designed algorithm to match patients with nearby clinical trials and created a web scraper to crawl trial eligibility criteria
-            </li>
-          </ul>
-        </div>
-      </div>
+      //   <ResumeHeading
+      //     heading={"Software Engineer / AI Engineer"}
+      //     subHeading={"Columbia Business School Startup Incubator"}
+      //     fromDate={"September 2022"}
+      //     toDate={"May 2024"}
+      //     description={
+      //       "Worked as a developer for two startups at the Columbia Business School"
+      //     }
+      //   />
+      // <div className="experience-container">    
+      //   <div className="resume-description-text">
+      //     <ul>
+      //       <li>
+      //         Developed back-end for travel experience platform adopted by 15+ hotels using Node.js; built admin and guest authentication and created APIs for managing reservations and property inforomation; implemented front-end in React and utilized Redux to manage state for user login and recommendations
+      //       </li>
+      //       <li>
+      //         Developed mobile app using React Native for non-English speaking patients to discover accessible clinical trials; designed algorithm to match patients with nearby clinical trials and created a web scraper to crawl trial eligibility criteria
+      //       </li>
+      //     </ul>
+      //   </div>
+      // </div>
     </div>,
     <div // Programming container
       className="resume-screen-container programming-skills-container"
